9c44484ca2d988fd4b3f2814a1c013c3b99e6da0
Ajay Singh Negi <negisinghajayus@gmail.com>
2024-10-23 11:15:30 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int shortest_lines_index(int[] lines, int n) {
+        int j;
+        int shortest = 0;
+        for (j = 1; j < n; j++)
+            if (lines[j] < lines[shortest])
+                shortest = j;
+        return shortest;
+    }
+
+    public static void solve(int[] lines, int n, int m) {
+        int i, shortest;
+        for (i = 0; i < m; i++) {
+            shortest = shortest_lines_index(lines, n);
+            System.out.println(lines[shortest]);
+            lines[shortest]++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] lines = new int[100];
+        int n, m, i;
+        Scanner keyboard = new Scanner(System.in);
+        n = keyboard.nextInt();
+        m = keyboard.nextInt();
+        for (i = 0; i < n; i++)
+            lines[i] = keyboard.nextInt();
+        solve(lines, n, m);
+        return;
+    }
+}
