92f281f73ee81df9fb81966e69386681576e88c2
Ajay Singh Negi <negisinghajayus@gmail.com>
2024-10-26 11:45:38 +0530

    Initial commit

--- src/Snowflakes.java
+++ src/Snowflakes.java
@@ -1,62 +1,79 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Snowflakes {
-    static int[][] snowflakes = new int[100000][6];
 
-    public static int identical_right(int[] snow1, int[] snow2, int start) {
-        int offset;
-        for (offset = 0; offset < 6; offset++) {
-            if (snow1[offset] != snow2[(start + offset) % 6])
-                return 0;
-        }
-        return 1;
+    static final int SIZE = 100000;
+
+    public static class Snowflake_Node {
+        int[] snowflake = new int[6];
+        Snowflake_Node next;
     }
 
-    public static int identical_left(int[] snow1, int[] snow2, int start) {
-        int offset, snow2_index;
-        for (offset = 0; offset < 6; offset++) {
-            snow2_index = start - offset;
-            if (snow2_index <= -1)
-                snow2_index = snow2_index + 6;
-            if (snow1[offset] != snow2[snow2_index])
-                return 0;
-        }
-        return 1;
+    public static int code(int[] snowflake) {
+        return (snowflake[0] + snowflake[1] + snowflake[2]
+                + snowflake[3] + snowflake[4] + snowflake[5]) % SIZE;
     }
 
-    public static int are_identical(int[] snow1, int[] snow2) {
-        int start;
-        for (start = 0; start < 6; start++) {
-            if (identical_right(snow1, snow2, start) == 1)
-                return 1;
-            if (identical_left(snow1, snow2, start) == 1)
-                return 1;
+    public static boolean identicalRight(int[] snow1, int[] snow2, int start) {
+        for (int offset = 0; offset < 6; offset++) {
+            if (snow1[offset] != snow2[(start + offset) % 6]) {
+                return false;
+            }
         }
-        return 0;
+        return true;
     }
 
-    public static void identify_identical(int[][] snowflakes, int n) {
-        int i, j;
-        for (i = 0; i < n; i++) {
-            for (j = i + 1; j < n; j++) {
-                if (are_identical(snowflakes[i], snowflakes[j]) == 1) {
-                    System.out.println("Twin snowflakes found.");
-                    return;
+    public static boolean identicalLeft(int[] snow1, int[] snow2, int start) {
+        for (int offset = 0; offset < 6; offset++) {
+            int snow2Index = (start - offset + 6) % 6;
+            if (snow1[offset] != snow2[snow2Index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean areIdentical(int[] snow1, int[] snow2) {
+        for (int start = 0; start < 6; start++) {
+            if (identicalRight(snow1, snow2, start) || identicalLeft(snow1, snow2, start)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void identifyIdentical(Snowflake_Node[] snowflakes) {
+        for (int i = 0; i < SIZE; i++) {
+            Snowflake_Node node1 = snowflakes[i];
+            while (node1 != null) {
+                Snowflake_Node node2 = node1.next;
+                while (node2 != null) {
+                    if (areIdentical(node1.snowflake, node2.snowflake)) {
+                        System.out.println("Twin snowflakes found.");
+                        return;
+                    }
+                    node2 = node2.next;
                 }
+                node1 = node1.next;
             }
         }
         System.out.println("No two snowflakes are alike.");
     }
 
     public static void main(String[] args) {
-        int n, i, j;
         Scanner keyboard = new Scanner(System.in);
-        n = keyboard.nextInt();
-        for (i = 0; i < n; i++)
-            for (j = 0; j < 6; j++)
-                snowflakes[i][j] = keyboard.nextInt();
-        identify_identical(snowflakes, n);
-        return;
+        Snowflake_Node[] snowflakes = new Snowflake_Node[SIZE];
+        int n = keyboard.nextInt();
+        for (int i = 0; i < n; i++) {
+            Snowflake_Node snow = new Snowflake_Node();
+            for (int j = 0; j < 6; j++) {
+                snow.snowflake[j] = keyboard.nextInt();
+            }
+            int snowflakeCode = code(snow.snowflake);
+            snow.next = snowflakes[snowflakeCode];
+            snowflakes[snowflakeCode] = snow;
+        }
+        identifyIdentical(snowflakes);
+        keyboard.close();
     }
 }
-
