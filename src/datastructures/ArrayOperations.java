639d3c6f0d6d0c74906d6e4a874d20892033f17d
Ajay Singh Negi <negisinghajayus@gmail.com>
2024-10-30 09:27:26 +0530

    Initial commit

--- src/datastructures/ArrayOperations.java
+++ src/datastructures/ArrayOperations.java
@@ -1,7 +1,29 @@
 package datastructures;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class ArrayOperations {
     public static final int MAX = 5;
+
+    public static void search(int[] arr, int num) {
+        int i;
+        for (i = 0; i < MAX; i++)
+            if (arr[i] == num)
+                System.out.println("Element " + num + " is at " + (i + 1) + "th position");
+    }
+
+    public static void reverse(int[] arr) {
+        int i;
+        for (i = 0; i < MAX/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[MAX-1-i];
+            arr[MAX-1-i] = temp;
+        }
+    }
+
+    public static void del(int[] arr, int pos) {
+        arr[pos-1] = 0;
+    }
 
     public static void display(int[] arr) {
         int i;
@@ -9,6 +31,7 @@ public class ArrayOperations {
             System.out.print(arr[i] + "\t");
         System.out.println();
     }
+
 
     public static void insert(int[] arr, int pos, int num) {
         arr[pos-1] = num;
@@ -23,6 +46,12 @@ public class ArrayOperations {
         insert ( arr, 5, 15 ) ;
         System.out.println("Elements of Array:");
         display(arr);
+        del(arr, 5);
+        reverse(arr);
+        System.out.println("After reversing");
+        display(arr);
+        search(arr, 0);
+        search(arr, 11);
     }
 
 }
