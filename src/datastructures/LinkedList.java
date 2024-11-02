c5e799bf359f546748810fe3ae475c2426d0afbf
Ajay Singh Negi <negisinghajayus@gmail.com>
2024-11-02 23:16:36 +0530

    Initial commit

--- src/datastructures/LinkedList.java
+++ src/datastructures/LinkedList.java
@@ -6,33 +6,79 @@ public class LinkedList {
         Node link;
     }
 
-    public static void addAfter(Node q, int)
-
-    public static void addAtBeg(Node q, int num) {
-        Node temp;
-        temp = new Node();
-        temp.data = num;
-        temp.link = q;
-        q = temp;
+    public static Node del(Node q, int num) {
+        Node old = null, temp = q;
+        while (temp != null) {
+            if (temp.data == num) {
+                if (temp == q)
+                    q = temp.link;
+                else
+                    old.link = temp.link;
+                return q;
+            }
+            else {
+                old = temp;
+                temp = temp.link;
+            }
+        }
+        System.out.println("Element " + num + " not found");
+        return q;
     }
 
-    public static void append(Node q, int num) {
+    public static void display(Node q) {
+        while (q != null) {
+            System.out.print(q.data + " ");
+            q = q.link;
+        }
+        System.out.println();
+    }
+
+    public static Node addAfter(Node q, int loc, int num) {
         Node temp, r;
-        if(q == null) {
-            temp = new Node();
-            temp.data = num;
-            temp.link = null;
-            q = temp;
+        int i;
+        temp = q;
+        for (i = 0; i < loc; i++) {
+            temp = temp.link;
+            if (temp == null) {
+                System.out.println("There are less than " + loc + " elements in list");
+                return q;
+            }
+            temp = temp.link;
+        }
+        r = new Node();
+        r.data = num;
+        r.link = temp.link;
+        temp.link = r;
+        return q;
+    }
+
+    public static Node addAtBeg(Node q, int num) {
+        Node temp = new Node();
+        temp.data = num;
+        temp.link = null;
+        Node r = new Node();
+        r = q;
+        q = temp;
+        q.link = r;
+        return q;
+    }
+
+    public static Node append(Node q, int num) {
+        if (q == null) {
+            q = new Node();
+            q.data = num;
+            q.link = null;
         }
         else {
-            temp = q;
-            while(temp.link!=null)
-                temp = temp.link;
-            r = new Node();
-            r.data = num;
-            r.link = null;
-            temp.link = r;
+            Node temp = new Node();
+            temp.data = num;
+            temp.link = null;
+            Node r = q;
+            while (r.link != null)
+                r = r.link;
+            r.link = temp;
         }
+        return q;
     }
 
     public static int count(Node q) {
@@ -45,20 +91,27 @@ public class LinkedList {
     }
 
     public static void main(String[] args) {
-        Node p;
-        p = null;
+        Node p = null;
         System.out.println("No. of elements in the linked List = " + count(p));
-        append(p, 14); append(p, 30); append(p, 25);
-        append(p, 42); append(p, 17);
+        p = append(p, 14);
+        p = append(p, 30);
+        p = append(p, 25);
+        p = append(p, 42);
+        p = append(p, 17);
         display(p);
-        addAtBeg(p, 99); addAtBeg(p, 88); addAtBeg(p, 77);
+        p = addAtBeg(p, 99);
+        p = addAtBeg(p, 88);
+        p = addAtBeg(p, 77);
         display(p);
-        addAfter(p, 3, 41); addAfter(p, 6, 89); addAfter(p, 10, 60);
+        p = addAfter(p, 3, 41);
+        p = addAfter(p, 6, 89);
+        p = addAfter(p, 10, 60);
         display(p);
         System.out.println("No. of elements in the linked List = " + count(p));
-        del(p, 99); del(p, 42); del(p, 10);
+        p = del(p, 99);
+        p = del(p, 42);
+        p = del(p, 10);
         display(p);
         System.out.println("No. of elements in the linked List = " + count(p));
     }
-
 }
