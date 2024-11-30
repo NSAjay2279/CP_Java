2790e70d63f86f0dae131746be919989299f635b
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-11-30 19:06:35 +0000

    updated...files

--- hello/app.py
+++ hello/app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        name = request.form.get("name", "world")
+        return render_template("greet.html", name=name)
     return render_template("index.html")
 
 
-@app.route("/greet")
-def greet():
-    name = request.args.get("name", None)
-    return render_template("greet.html", name=name)
+
 
